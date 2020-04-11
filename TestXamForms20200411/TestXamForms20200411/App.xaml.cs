3ef39cc05ef45ef2e3902acf813da8ae8f5edc81
mkamoski <mkamoski@users.noreply.github.com>
2020-04-11 15:22:56 -0400

    Add project files.

---
+++ TestXamForms20200411/TestXamForms20200411/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TestXamForms20200411.Services;
+using TestXamForms20200411.Views;
+
+namespace TestXamForms20200411
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TestXamForms20200411/TestXamForms20200411/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TestXamForms20200411.Models;
+
+namespace TestXamForms20200411.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
